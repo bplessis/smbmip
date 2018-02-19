27bdf4addb880b049a4ecf8ca24357037084aefb
Benoit Plessis <b.plessis@doyousoft.com>
2018-02-19 16:32:19 +0100

    Use JSON output

--- smbmip.go
+++ smbmip.go
@@ -1,42 +1,58 @@
 package main
 
 import (
-	"strconv"
+	"encoding/json"
 	"fmt"
 	"net"
 	"os"
+	"strconv"
 )
 
 func main() {
-	var port int = 8080 ;
+	var port int = 8080
 
-	ln, err := net . Listen("tcp", ":" + strconv.Itoa( port ) )
+	ln, err := net.Listen("tcp", ":"+strconv.Itoa(port))
 	if err != nil {
 		// handle error
-		fmt . Println("Error listening:", err.Error())
-		os . Exit(1)
+		fmt.Println("Error listening:", err.Error())
+		os.Exit(1)
 	}
-	defer ln . Close ()
+	defer ln.Close()
 
-	fmt . Println ( "Listening on port " + strconv.Itoa ( port ) )
+	fmt.Println("Listening on port " + strconv.Itoa(port))
 	for {
 		conn, err := ln.Accept()
 		if err != nil {
 			// handle error
-			fmt . Println ( "Error accepting connection : " + err.Error () )
-			os . Exit (1)
+			fmt.Println("Error accepting connection : " + err.Error())
+			os.Exit(1)
 		}
 		go handleConnection(conn)
 	}
 }
 
+type Connection struct {
+	Name        string      `json:"marbuk"`
+	IP          net.IP      `json:"ip"`
+}
+
 // Handles incoming requests.
 func handleConnection(conn net.Conn) {
-	// Send a response back to person contacting us.
-	var ls = conn . LocalAddr () . (*net.TCPAddr)
-	var lc = conn . RemoteAddr () . (*net.TCPAddr)
+	var thisConn Connection
 
-	conn . Write( []byte("Message received from " + lc.IP.String() + " to " + ls.IP.String() ) )
+	thisConn.Name = "sbmip"
+
+	thisConn.IP = conn.RemoteAddr().(*net.TCPAddr).IP
+
+	// Send a response back to person contacting us.
+	b, err := json.Marshal(thisConn)
+	if err != nil {
+		conn.Write([]byte("Message received from " + thisConn.IP.String()))
+		fmt.Println("JSON Conversion error: " + err.Error())
+	} else {
+		conn.Write(b)
+	}
+
 	// Close the connection when you're done with it.
-	conn . Close()
+	conn.Close()
 }
